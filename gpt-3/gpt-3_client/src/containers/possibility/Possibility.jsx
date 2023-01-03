fc77933fdc79bccebd558a72a81be2184fac48ec
tayoolaigbe <tayoolaigbe@outlook.com>
2023-01-03 19:39:24 +0000

    Caught the minor error in possibility.jsx with the h1 tag

--- gpt-3/gpt-3_client/src/containers/possibility/Possibility.jsx
+++ gpt-3/gpt-3_client/src/containers/possibility/Possibility.jsx
@@ -11,11 +11,11 @@ const Possibility = () => {
 			</div>
 			<div className="gpt3__possibility-content">
 				<h4>Request Early Access to Get Started</h4>
-				<h1 className="gradient-text">
+				<h1 className="gradient__text">
 					The possibilities are beyond your imagination
 				</h1>
 				<p>
-					Yet bed any for travelling assistance indulgence unpleasing. Not
+					Yet bed any for traveling assistance indulgence unpleasing. Not
 					thoughts all exercise blessing. Indulgence way everything joy
 					alteration boisterous the attachment. Party we years to order allow
 					asked of.
