fe312bb57e055ecc03aaa58c128d92359dc67bd8
tayoolaigbe <tayoolaigbe@outlook.com>
2023-01-03 11:20:03 +0000

    Added my assests folder. Still Working on the Navbar

--- gpt-3/gpt-3_client/src/components/navbar/Navbar.jsx
+++ gpt-3/gpt-3_client/src/components/navbar/Navbar.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css';
 
+import logo from '../../assets/logo.svg';
+
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<div className="gpt__navbar">
+			<div className="gpt3__navbar-links">
+				<div className="gpt3__navbar-links_logo">
+					<img src={logo} alt="logo" />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
