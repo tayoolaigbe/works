6c4e779ac1daa24231a50e765b837ecf1a8b1bdb
tayoolaigbe <tayoolaigbe@outlook.com>
2023-01-07 21:04:52 +0000

    Fixed the error in SpecialMenu.jsx

--- gericht/gericht/src/container/Menu/SpecialMenu.jsx
+++ gericht/gericht/src/container/Menu/SpecialMenu.jsx
@@ -7,11 +7,11 @@ const SpecialMenu = () => (
 	<div className="app__specialMenu flex__center section__padding" id="menu">
 		<div className="app__specialMenu-title">
 			<SubHeading title="Menu that fits your Palatte" />
-			<h1 className="text__cormorant">Today's Special</h1>
+			<h1 className="headtext__cormorant">Today's Special</h1>
 		</div>
 		<div className="app__specialMenu-menu">
 			<div className="app__specialMenu-menu_wine flex__center">
-				<p className="app__specialMenu_menu_heading">Wine & Beer</p>
+				<p className="app__specialMenu-menu_heading">Wine & Beer</p>
 				<div className="app__specialMenu_menu_items">
 					{data.wines.map((wine, index) => (
 						<MenuItem
@@ -27,7 +27,7 @@ const SpecialMenu = () => (
 				<img src={images.menu} alt="menu_img" />
 			</div>
 			<div className="app__specialMenu-menu_cocktails flex__center">
-				<p className="app__specialMenu_menu_heading">Wine & Beer</p>
+				<p className="app__specialMenu-menu_heading">Coctails</p>
 				<div className="app__specialMenu_menu_items">
 					{data.cocktails.map((cocktail, index) => (
 						<MenuItem
