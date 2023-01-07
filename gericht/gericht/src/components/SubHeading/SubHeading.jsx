0436008c031167b5a97c0c21f1f6d4d1af06f848
tayoolaigbe <tayoolaigbe@outlook.com>
2023-01-07 19:36:07 +0000

    WOrked on the re-usable component SubHeading

--- gericht/gericht/src/components/SubHeading/SubHeading.jsx
+++ gericht/gericht/src/components/SubHeading/SubHeading.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import images from '../../constants/images';
+
 const SubHeading = () => (
-  <div>
-    SubHeading
-  </div>
+	<div style={{ marginBotom: '1rem' }}>
+		<p className="p__cormorant">TITLE</p>
+		<img src={images.spoon} alt="spoon" className="spoon_img" />
+	</div>
 );
 
 export default SubHeading;
