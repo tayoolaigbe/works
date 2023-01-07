d41571335e366c373bf969cbe6fcfe9ea0dd68d0
tayoolaigbe <tayoolaigbe@outlook.com>
2023-01-07 12:25:45 +0000

    Implemented the apps design in app.css and index.css files

--- gericht/gericht/src/App.jsx
+++ gericht/gericht/src/App.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 
+import {
+	AboutUs,
+	Chef,
+	FindUs,
+	Footer,
+	Gallery,
+	Header,
+	Intro,
+	Laurels,
+	SpecialMenu,
+} from './container';
+
+import { Navbar } from './components';
+
 import './App.css';
 
 function App() {
 	return (
 		<div className="App">
-			<h1>App</h1>
+			<Navbar />
+			<Header />
+			<AboutUs />
+			<SpecialMenu />
+			<Chef />
+			<Intro />
+			<Laurels />
+			<Gallery />
+			<FindUs />
+			<Footer />
 		</div>
 	);
 }
