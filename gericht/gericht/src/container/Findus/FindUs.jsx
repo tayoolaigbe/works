6211d7094ad7341cc4557afb7e4a1675e73e5c23
tayoolaigbe <tayoolaigbe@outlook.com>
2023-01-08 17:08:50 +0000

    FIxed contact us in findUs.jsx

--- gericht/gericht/src/container/Findus/FindUs.jsx
+++ gericht/gericht/src/container/Findus/FindUs.jsx
@@ -5,7 +5,7 @@ import { images, data } from '../../constants';
 const FindUs = () => (
 	<div className="app__bg app__wrapper section__padding" id="contact">
 		<div className="app__wrapper_info">
-			<SubHeading title="COntact" />
+			<SubHeading title="Contact" />
 			<h1 className="headtext__cormorant" style={{ marginBottom: '3rem' }}>
 				Find Us
 			</h1>
