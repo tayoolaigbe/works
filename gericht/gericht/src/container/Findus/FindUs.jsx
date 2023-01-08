816646ee3dbd42dcb80644522763a64c80c03404
tayoolaigbe <tayoolaigbe@outlook.com>
2023-01-08 14:56:27 +0000

    COmpleted the FindUs container

--- gericht/gericht/src/container/Findus/FindUs.jsx
+++ gericht/gericht/src/container/Findus/FindUs.jsx
@@ -22,6 +22,13 @@ const FindUs = () => (
 				<p className="p__opensans">Mon - Fri: 10:00 am - 02:00 am</p>
 				<p className="p__opensans">Sat - Sun: 10:00 am - 03:00 am</p>
 			</div>
+			<button
+				type="button"
+				className="custom__button"
+				style={{ marginTop: '2rem' }}
+			>
+				Visit Us
+			</button>
 		</div>
 		<div className="app__wrapper_img">
 			<img src={images.findus} alt="find us" />
