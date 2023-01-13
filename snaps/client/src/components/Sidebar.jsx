921f656607e39dad988b1ac20f39bb6f24146fdd
tayoolaigbe <tayoolaigbe@outlook.com>
2023-01-13 22:13:50 +0000

    Working on th eSidebar component

--- snaps/client/src/components/Sidebar.jsx
+++ snaps/client/src/components/Sidebar.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import { IoIosArrowForward } from 'react-icons/io';
+import { RiHomeFill } from 'react-icons/ri';
+import { Link, NavLink } from 'react-router-dom';
+import logo from '../assets/logo.png';
 
-const Sidebar = () => {
-	return <div>Sidebar</div>;
+const isNotActiveStyle =
+	'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
+const isActiveStyle =
+	'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black  transition-all duration-200 ease-in-out capitalize';
+
+const Sidebar = ({ user, closeToggle }) => {
+	const handleCloseSidebar = () => {
+		if (closeToggle) closeToggle(false);
+	};
+	return (
+		<div className="flex flex-col justify-between bg-white h-full overflow-y-scroll min-w-210 hide-scrollbar">
+			<div className="flex flex-col">
+				<Link
+					to="/"
+					className="flex px-5 gap-2 my-6 pt-1 w-190 items-center"
+					onClick={handleCloseSidebar}
+				>
+					<img src={logo} alt="logo" className="w-full" />
+				</Link>
+				<div className="flex flex-col gap-5">
+					<NavLink
+						to="/"
+						className={({ isActive }) =>
+							isActive ? isActiveStyle : isNotActiveStyle
+						}
+					></NavLink>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Sidebar;
