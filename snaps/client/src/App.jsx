56aef73fd2fc00c5ca0c36abfa29649ffad96519
tayoolaigbe <tayoolaigbe@outlook.com>
2023-01-13 00:05:36 +0000

    Added Router to main and app.jsx

--- snaps/client/src/App.jsx
+++ snaps/client/src/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 
 const App = () => {
-	return <div className="text-3xl font-bold underline">App</div>;
+	return (
+		<Routes>
+			<Route path="login" element={<Login />} />
+			<Route path="/*" element={<Home />} />
+		</Routes>
+	);
 };
 
 export default App;
