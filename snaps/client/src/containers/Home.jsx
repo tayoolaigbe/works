64a467e0ef16d6a84f61181eee55d03258be3357
tayoolaigbe <tayoolaigbe@outlook.com>
2023-01-13 21:16:37 +0000

    Added the Sidebar and the UserProfile components

--- snaps/client/src/containers/Home.jsx
+++ snaps/client/src/containers/Home.jsx
@@ -1,4 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { HiMenu } from 'react-icons/hi';
+import { AiFillCloseCircle } from 'react-icons/ai';
+import { Link, Route, Routes } from 'react-router-dom';
+
+import { Sidebar, UserProfile } from '../components';
 
 const Home = () => {
 	return <div>Home</div>;
