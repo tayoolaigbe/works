cdf2551f0edb598dfda3c9f3e6449d5c1e6768a6
tayoolaigbe <tayoolaigbe@outlook.com>
2023-01-13 00:13:50 +0000

    Added the Home and Login container and components respectively

--- snaps/client/src/App.jsx
+++ snaps/client/src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Route, Routes, useNavigate } from 'react-router-dom';
+import Login from './components/Login';
+import Home from './containers/Home';
 
 const App = () => {
 	return (
