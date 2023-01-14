5abd9a7fc1648d2f450cd5f95388bab70850c21d
tayoolaigbe <tayoolaigbe@outlook.com>
2023-01-14 15:25:32 +0000

    Working on the Feed component. Added the Spinner and the MasonryLayout components as well.

--- snaps/client/src/components/Feed.jsx
+++ snaps/client/src/components/Feed.jsx
@@ -1,4 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { client } from '../client';
+import MasonryLayout from './MasonryLayout';
 
 const Feed = () => {
 	return <div>Feed</div>;
