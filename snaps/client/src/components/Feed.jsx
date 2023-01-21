7364c3bfa132be916e95c36aff744f1528001aa4
tayoolaigbe <tayoolaigbe@outlook.com>
2023-01-21 17:59:19 +0000

    Worked on the Feed and Spinner jsx components

--- snaps/client/src/components/Feed.jsx
+++ snaps/client/src/components/Feed.jsx
@@ -3,8 +3,12 @@ import { useParams } from 'react-router-dom';
 
 import { client } from '../client';
 import MasonryLayout from './MasonryLayout';
+import Spinner from './Spinner';
 
 const Feed = () => {
+	const [loading, setLoading] = useState(false);
+	if (loading)
+		return <Spinner message="We are adding new ideas to your feed!" />;
 	return <div>Feed</div>;
 };
 
