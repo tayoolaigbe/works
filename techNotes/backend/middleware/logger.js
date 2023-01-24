886dad5cd6b9333622e21b78a88e45c6cf8f0112
tayoolaigbe <tayoolaigbe@outlook.com>
2023-01-24 21:11:08 +0000

    fixed import error in logger middleware

--- techNotes/backend/middleware/logger.js
+++ techNotes/backend/middleware/logger.js
@@ -1,4 +1,4 @@
-const { format } = require('format-fns');
+const { format } = require('date-fns');
 const { v4: uuid } = require('uuid');
 const fs = require('fs');
 const fsPromises = require('fs').promises;
