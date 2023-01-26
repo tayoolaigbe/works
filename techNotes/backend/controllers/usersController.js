220e2e16411beaaf81793b58aedee24efe0ebc80
tayoolaigbe <tayoolaigbe@outlook.com>
2023-01-26 19:13:37 +0000

    Working on the userController methods

--- techNotes/backend/controllers/usersController.js
+++ techNotes/backend/controllers/usersController.js
@@ -2,17 +2,34 @@ const User = require('../models/User');
 const Note = require('../models/Note');
 const asyncHandler = require('express-async-handler');
 const bcrypt = require('bcrypt');
+const { findOne } = require('../models/Note');
 
 // @desc Get all users
 // @route GET /users
 // @access private
 
-const getAllUsers = asyncHandler(async (req, res) => {});
+const getAllUsers = asyncHandler(async (req, res) => {
+	const users = await User.find().select('-password').lean();
+	if (!users) {
+		return res.status(400).json({ message: 'No Users Found' });
+	}
+	res.json(users);
+});
 
 // @desc create new user
 // @route POST /users
 // @access private
-const createNewUser = asyncHandler(async (req, res) => {});
+const createNewUser = asyncHandler(async (req, res) => {
+	const { username, password, roles } = req.body;
+
+	// Confirm data
+	if (!username || !password || !Array.isArray(roles) || !roles.length) {
+		return res.status(400).json({ message: 'All fields are Required' });
+	}
+
+	// Check for duuplicates
+	const duplicate = await findOne({ username }).lean().exec();
+});
 
 // @desc update a user
 // @route PATCH /users
@@ -23,3 +40,5 @@ const updateUser = asyncHandler(async (req, res) => {});
 // @route DELETE /users
 // @access private
 const deleteUser = asyncHandler(async (req, res) => {});
+
+module.exports = { getAllUsers, createNewUser, updateUser, deleteUser };
