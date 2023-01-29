881913c615079e5efcb83d9481b1264dfec2dfc3
tayoolaigbe <tayoolaigbe@outlook.com>
2023-01-29 09:32:44 +0000

    Worked on the deleteUser function.

--- techNotes/backend/controllers/usersController.js
+++ techNotes/backend/controllers/usersController.js
@@ -96,6 +96,28 @@ const updateUser = asyncHandler(async (req, res) => {
 // @desc delete a user
 // @route DELETE /users
 // @access private
-const deleteUser = asyncHandler(async (req, res) => {});
+const deleteUser = asyncHandler(async (req, res) => {
+	const { id } = req.body;
+
+	if (!id) {
+		return res.status(400).json({ message: 'User iD Required' });
+	}
+
+	const notes = await Note.findOne({ user: id }).lean().exec();
+	if (notes?.length) {
+		return res.status(400).json({ message: 'User has assigned Notes' });
+	}
+
+	const user = await User.findById(id).exec();
+
+	if (!user) {
+		return res.status(400).json({ message: 'User NOt Found' });
+	}
+
+	const result = await user.deleteOne();
+
+	const reply = `Username ${result.username} with ID ${result._id} deleted `;
+	res.json(reply);
+});
 
 module.exports = { getAllUsers, createNewUser, updateUser, deleteUser };
