8ca366e115dbd125811de4854d51ff0f8896e1f0
tayoolaigbe <tayoolaigbe@outlook.com>
2023-02-04 22:09:06 +0000

    Worked on the return of the User.jsx

--- techNotes/frontend/src/features/users/User.jsx
+++ techNotes/frontend/src/features/users/User.jsx
@@ -14,6 +14,20 @@ const USer = ({ userId }) => {
 		const handleEdit = () => navigate(`/dash/users/${userId}`);
 
 		const userRolesString = user.roles.toString().replaceAll(',', ', ');
+
+		const cellStatus = user.active ? '' : 'table__cell--inactive';
+
+		return (
+			<tr className="table__row user">
+				<td className={`table__cell ${cellStatus}`}>{user.username}</td>
+				<td className={`table__cell ${cellStatus}`}>{userRolesString}</td>
+				<td className={`table__cell ${cellStatus}`}>
+					<button className="icon-button table__button" onClick={handleEdit}>
+						<FontAwesomeIcon icon={faPenToSquare} />
+					</button>
+				</td>
+			</tr>
+		);
 	} else {
 		return null;
 	}
