d52f88dabf34a94ec4ad938e592974f997fbb29b
tayoolaigbe <tayoolaigbe@outlook.com>
2023-02-12 11:51:55 +0000

    Installed jsonwebtoken, completed the authController and used them alongside loginLimiter in authRoutes

--- techNotes/backend/routes/authRoutes.js
+++ techNotes/backend/routes/authRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const loginLimiter = require('../middleware/loginLimiter');
 
-router.route('/').post();
-router.route('/refresh').get();
-router.route('/logout').post();
+router.route('/').post(loginLimiter, authController.login);
+router.route('/refresh').get(authController.refresh);
+router.route('/logout').post(authController.logout);
 
 module.exports = router;
