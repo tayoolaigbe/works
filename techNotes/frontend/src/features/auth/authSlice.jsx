debf0d37c34ed97e03e2aff724a8510302850692
tayoolaigbe <tayoolaigbe@outlook.com>
2023-02-13 19:31:19 +0000

    Added the authSlice.jsx to work with redux in the frontend

---
+++ techNotes/frontend/src/features/auth/authSlice.jsx
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const authSlice = createSlice({
+	name: 'auth',
+	initialState: { token: null },
+	reducers: {
+		setCredentials: (state, action) => {
+			const { accessToken } = action.payload;
+			state.token = accessToken;
+		},
+		logOut: (state, action) => {
+			state.token = null;
+		},
+	},
+});
