bbf37fb544863c2ea042e4c6e395a9d88efaee98
tayoolaigbe <tayoolaigbe@outlook.com>
2023-02-14 23:27:10 +0000

    Completed the DashHeader.jsx by adding buttons and functionalities

--- techNotes/frontend/src/components/DashHeader.jsx
+++ techNotes/frontend/src/components/DashHeader.jsx
@@ -24,15 +24,33 @@ const DashHeader = () => {
 
 	if (isLoading) return <p>Logging Out...</p>;
 
-	if (isError) return <p>Error: {error.message}</p>;
+	if (isError) return <p>Error: {error.data.message}</p>;
+
+	let dashClass = null;
+	if (
+		!DASH_REGEX.test(pathname) &&
+		!NOTES_REGEX.test(pathname) &&
+		!USERS_REGEX.test(pathname)
+	) {
+		dashClass = 'dash-header__container--small';
+	}
+
+	const logoutButton = (
+		<button className="icon-button" title="Logout" onClick={sendLogout}>
+			<FontAwesomeIcon icon={faRightFromBracket} />
+		</button>
+	);
 
 	const content = (
 		<header className="dash-header">
-			<div className="dash-header__container">
+			<div className={`dash-header__container ${dashClass}`}>
 				<Link to="/dash">
 					<h1 className="dash-header__title">techNotes</h1>
 				</Link>
-				<nav className="dash-header__nav">{/* add nav buttons later */}</nav>
+				<nav className="dash-header__nav">
+					{/* add more buttons later */}
+					{logoutButton}
+				</nav>
 			</div>
 		</header>
 	);
