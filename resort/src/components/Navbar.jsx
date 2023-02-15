e85715fc3f549b37713e43ed77a21288433360b8
tayoolaigbe <tayoolaigbe@outlook.com>
2023-02-15 22:57:50 +0000

    Comment some properties out in App.css .navbar and aslo started working on the Navbar component

--- resort/src/components/Navbar.jsx
+++ resort/src/components/Navbar.jsx
@@ -1,7 +1,33 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import logo from '../assets/images/logo.svg';
+import { FaAlignRight } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
-export default class Navbar extends Component {
-	render() {
-		return <div>Hello from Navbar</div>;
-	}
-}
+// export default class Navbar extends Component {
+// 	render() {
+// 		return <div>Hello from Navbar</div>;
+// 	}
+// }
+
+import React, { useState } from 'react';
+
+const Navbar = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const handleToggle = () => {
+		setIsOpen(isOpen => !isOpen);
+	};
+	return (
+		<nav className="navbar">
+			<div className="nav-center">
+				<div className="nav-header">
+					<Link to="/">
+						<img src={logo} alt="beach-resort-log" />
+					</Link>
+				</div>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
