e3954f175b3c5be228260c3dd71aaa62d597d9f1
tayoolaigbe <tayoolaigbe@outlook.com>
2023-02-16 15:03:45 +0000

    COmpleted the Services component with state data

--- resort/src/components/Services.jsx
+++ resort/src/components/Services.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from 'react-icons/fa';
 import Title from './Title';
 
 const Services = () => {
+	const [services, setServices] = useState([
+		{
+			icon: <FaCocktail />,
+			title: 'Free Cocktails',
+			info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur, quas!',
+		},
+		{
+			icon: <FaHiking />,
+			title: 'Endless Hiking',
+			info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur, quas!',
+		},
+		{
+			icon: <FaShuttleVan />,
+			title: 'Free Shuttle',
+			info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur, quas!',
+		},
+		{
+			icon: <FaBeer />,
+			title: 'Strongest Beers',
+			info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur, quas!',
+		},
+	]);
 	return (
-		<div>
+		<section className="services">
 			<Title title="services" />
-		</div>
+			<div className="services-center">
+				{services.map((service, index) => {
+					return (
+						<article key={`service-${index}`} className="service">
+							<span>{service.icon}</span>
+							<h6>{service.title}</h6>
+							<p>{service.info}</p>
+						</article>
+					);
+				})}
+			</div>
+		</section>
 	);
 };
 
