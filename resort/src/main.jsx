2a49c358cb9ba2edb096ef606e74cbb3b37c0f93
tayoolaigbe <tayoolaigbe@outlook.com>
2023-02-16 22:43:01 +0000

    Used the RoomProvider in main.jsx

--- resort/src/main.jsx
+++ resort/src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import { RoomProvider } from './context';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<Router>
-			<App />
-		</Router>
+		<RoomProvider>
+			<Router>
+				<App />
+			</Router>
+		</RoomProvider>
 	</React.StrictMode>
 );
