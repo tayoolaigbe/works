961702305e403a7d46f35f1d2f242d7aa03d06d8
tayoolaigbe <tayoolaigbe@outlook.com>
2023-02-16 23:18:10 +0000

    Working on the state of the context

--- resort/src/components/FeaturedRooms.jsx
+++ resort/src/components/FeaturedRooms.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import { RoomContext } from '../context';
 
 export default class FeaturedRooms extends Component {
+	static contextType = RoomContext;
 	render() {
-		return <div>Hello from FeaturedRooms</div>;
+		// const value = this.context;
+		return <div>Hello from FeaturedRooms {value}</div>;
 	}
 }
--- resort/src/context.jsx
+++ resort/src/context.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import items from './data';
 
 const RoomContext = React.createContext();
 class RoomProvider extends Component {
-	state = {};
+	state = {
+		rooms: [],
+		sortedRooms: [],
+		featuredRooms: [],
+		loading: true,
+	};
 	render() {
 		return (
-			<RoomContext.Provider value="hello">
+			<RoomContext.Provider value={'hello'}>
 				{this.props.children}
 			</RoomContext.Provider>
 		);
