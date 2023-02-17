d479f3fa6122737ce04be6d43e468a65f993e654
tayoolaigbe <tayoolaigbe@outlook.com>
2023-02-17 23:01:56 +0000

    Working on the Room.jsx file

--- resort/src/components/Room.jsx
+++ resort/src/components/Room.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import defaultImg from '../assets/images/room-1.jpeg';
 
-const Room = () => {
-	return <div>Hello from Room</div>;
+const Room = ({ room }) => {
+	const { name, slug, images, price } = room;
+	return (
+		<article className="room">
+			<div className="img-container">
+				<img src={images[0] || defaultImg} alt={slug} />
+				<div className="price-top">
+					<h6>${price}</h6>
+					<p>per night</p>
+				</div>
+			</div>
+		</article>
+	);
 };
 
 export default Room;
