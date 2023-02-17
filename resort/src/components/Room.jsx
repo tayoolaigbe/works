a6564bf6bff3281862c3d943afaa4bd20a5d51fa
tayoolaigbe <tayoolaigbe@outlook.com>
2023-02-17 23:06:31 +0000

    Completed work on the Rooms.jsx

--- resort/src/components/Room.jsx
+++ resort/src/components/Room.jsx
@@ -12,6 +12,9 @@ const Room = ({ room }) => {
 					<h6>${price}</h6>
 					<p>per night</p>
 				</div>
+				<Link to={`/rooms/${slug}`} className="btn-primary room-link">
+					View room
+				</Link>
 			</div>
 		</article>
 	);
