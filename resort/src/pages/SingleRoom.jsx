1823307d1f5082c1c6420faa8634872118c8db16
tayoolaigbe <tayoolaigbe@outlook.com>
2023-02-18 14:28:11 +0000

    Installed styled-components and added StyledHero.jsx component. Still getting error of undefined params

--- resort/src/pages/SingleRoom.jsx
+++ resort/src/pages/SingleRoom.jsx
@@ -1,32 +1,33 @@
 import React, { Component } from 'react';
 import defaultBcg from '../assets/images/room-1.jpeg';
-import Hero from '../components/Hero';
+// import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import { Link } from 'react-router-dom';
 import { RoomContext } from '../context';
 
+import StyledHero from '../components/StyledHero';
 export default class SingleRoom extends Component {
 	constructor(props) {
 		super(props);
 		console.log(this.props);
 		this.state = {
 			slug: this.props.match.params.slug,
-			defaultBcg,
+			defaultBcg: defaultBcg,
 		};
 	}
-
-	// componentDidMount(){
-
-	// }
-
 	static contextType = RoomContext;
+
+	// componentDidMount() {
+	//   console.log(this.props);
+	// }
 	render() {
 		const { getRoom } = this.context;
 		const room = getRoom(this.state.slug);
+
 		if (!room) {
 			return (
 				<div className="error">
-					<h3>no such room found...</h3>
+					<h3> no such room could be found...</h3>
 					<Link to="/rooms" className="btn-primary">
 						back to rooms
 					</Link>
@@ -44,14 +45,51 @@ export default class SingleRoom extends Component {
 			pets,
 			images,
 		} = room;
+		const [main, ...defaultImages] = images;
+		console.log(defaultImages);
+
 		return (
-			<Hero hero="roomsHero">
-				<Banner title={`${name} room`}>
-					<Link to="/rooms" className="btn-primary">
-						back to rooms
-					</Link>
-				</Banner>
-			</Hero>
+			<>
+				<StyledHero img={images[0] || this.state.defaultBcg}>
+					<Banner title={`${name} room`}>
+						<Link to="/rooms" className="btn-primary">
+							back to rooms
+						</Link>
+					</Banner>
+				</StyledHero>
+				<section className="single-room">
+					<div className="single-room-images">
+						{defaultImages.map((item, index) => (
+							<img key={index} src={item} alt={name} />
+						))}
+					</div>
+					<div className="single-room-info">
+						<article className="desc">
+							<h3>details</h3>
+							<p>{description}</p>
+						</article>
+						<article className="info">
+							<h3>info</h3>
+							<h6>price : ${price}</h6>
+							<h6>size : {size} SQFT</h6>
+							<h6>
+								max capacity :
+								{capacity > 1 ? `${capacity} people` : `${capacity} person`}
+							</h6>
+							<h6>{pets ? 'pets allowed' : 'no pets allowed'}</h6>
+							<h6>{breakfast && 'free breakfast included'}</h6>
+						</article>
+					</div>
+				</section>
+				<section className="room-extras">
+					<h6>extras </h6>
+					<ul className="extras">
+						{extras.map((item, index) => (
+							<li key={index}>- {item}</li>
+						))}
+					</ul>
+				</section>
+			</>
 		);
 	}
 }
