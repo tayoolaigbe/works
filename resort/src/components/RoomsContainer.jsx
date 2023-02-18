703b14c86a61537a7025925026d8cbbf43fcce34
tayoolaigbe <tayoolaigbe@outlook.com>
2023-02-18 22:28:35 +0000

    Rendered the Components(RoomsFilter and RoomsList)

--- resort/src/components/RoomsContainer.jsx
+++ resort/src/components/RoomsContainer.jsx
@@ -3,7 +3,13 @@ import RoomsFilter from './RoomsFilter';
 import RoomsList from './RoomsList';
 
 const RoomsContainer = () => {
-	return <div>Hello from RoomsContainer</div>;
+	return (
+		<div>
+			Hello from RoomsContainer
+			<RoomsFilter />
+			<RoomsList />
+		</div>
+	);
 };
 
 export default RoomsContainer;
