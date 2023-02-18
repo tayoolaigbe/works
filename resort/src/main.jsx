9596913e1adeca52e2381bbe813fa9945dc94cdb
tayoolaigbe <tayoolaigbe@outlook.com>
2023-02-18 23:26:43 +0000

    Fixed the loading error by removing the <React.StrictMode> in main.jsx

--- resort/src/main.jsx
+++ resort/src/main.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { RoomProvider } from './context';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<RoomProvider>
-			<Router>
-				<App />
-			</Router>
-		</RoomProvider>
-	</React.StrictMode>
+	<RoomProvider>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</RoomProvider>
 );
