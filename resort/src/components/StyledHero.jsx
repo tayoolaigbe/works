a9930a29f339ff84c1fc5633c2f961176c115f49
tayoolaigbe <tayoolaigbe@outlook.com>
2023-02-18 14:53:26 +0000

    Fixed the error in StyledHero.jsx with the image import

--- resort/src/components/StyledHero.jsx
+++ resort/src/components/StyledHero.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import defaultImg from '../assets//images/room-1.jpeg';
+import defaultImg from '../assets/images/room-1.jpeg';
 const StyledHero = styled.header`
 	min-height: 60vh;
 	/* background: url(${defaultImg}); */
--- resort/src/pages/SingleRoom.jsx
+++ resort/src/pages/SingleRoom.jsx
@@ -9,7 +9,7 @@ import StyledHero from '../components/StyledHero';
 export default class SingleRoom extends Component {
 	constructor(props) {
 		super(props);
-		console.log(this.props);
+		// console.log(this.props);
 		this.state = {
 			slug: this.props.match.params.slug,
 			defaultBcg: defaultBcg,
