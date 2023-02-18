0576664d25b15515f07e9bcbcd57df05bd441d1a
tayoolaigbe <tayoolaigbe@outlook.com>
2023-02-18 13:47:56 +0000

    Working on the SingleRoom.jsx: trying to retrieve the slug params

--- resort/src/pages/SingleRoom.jsx
+++ resort/src/pages/SingleRoom.jsx
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
+import defaultBcg from '../assets/images/room-1.jpeg';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom';
+import { RoomContext } from '../context';
 
 export default class SingleRoom extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			slug: this.props.match.params.slug,
+			defaultBcg,
+		};
+	}
+
+	// componentDidMount(){
+
+	// }
+
+	static contextType = RoomContext;
 	render() {
 		return <div>Hello from SingleRoom</div>;
 	}
