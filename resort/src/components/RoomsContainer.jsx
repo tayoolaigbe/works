2a5a57f4bd470b99bf122073275ad35a6b39eb0a
tayoolaigbe <tayoolaigbe@outlook.com>
2023-02-19 08:54:31 +0000

    Using the context in RoomsContainer.jsx

--- resort/src/components/RoomsContainer.jsx
+++ resort/src/components/RoomsContainer.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import RoomsFilter from './RoomsFilter';
 import RoomsList from './RoomsList';
+import { RoomConsumer } from '../context';
+import Loading from './Loading';
 
 const RoomsContainer = () => {
 	return (
-		<div>
-			Hello from RoomsContainer
-			<RoomsFilter />
-			<RoomsList />
-		</div>
+		<RoomConsumer>
+			{value => {
+				return (
+					<div>
+						Hello from RoomsContainer
+						<RoomsFilter />
+						<RoomsList />
+					</div>
+				);
+			}}
+		</RoomConsumer>
 	);
 };
 
