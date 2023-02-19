ba482a01f8d6dfc47687062000feaf02057a07cd
tayoolaigbe <tayoolaigbe@outlook.com>
2023-02-19 09:21:59 +0000

    USed the higher order function in RoomsContainer.jsx

--- resort/src/components/RoomsContainer.jsx
+++ resort/src/components/RoomsContainer.jsx
@@ -1,27 +1,48 @@
 import React from 'react';
+import { withRoomConsumer } from '../context';
+import Loading from './Loading';
 import RoomsFilter from './RoomsFilter';
 import RoomsList from './RoomsList';
-import { RoomConsumer } from '../context';
-import Loading from './Loading';
 
-const RoomsContainer = () => {
+function RoomContainer({ context }) {
+	const { loading, sortedRooms, rooms } = context;
+	if (loading) {
+		return <Loading />;
+	}
 	return (
-		<RoomConsumer>
-			{value => {
-				const { loading, sortedRooms, rooms } = value;
-				if (loading) {
-					return <Loading />;
-				}
-				return (
-					<div>
-						Hello from RoomsContainer
-						<RoomsFilter rooms={rooms} />
-						<RoomsList rooms={sortedRooms} />
-					</div>
-				);
-			}}
-		</RoomConsumer>
+		<>
+			<RoomsFilter rooms={rooms} />
+			<RoomsList rooms={sortedRooms} />
+		</>
 	);
-};
+}
 
-export default RoomsContainer;
+export default withRoomConsumer(RoomContainer);
+
+// import React from 'react';
+// import RoomsFilter from './RoomsFilter';
+// import RoomsList from './RoomsList';
+// import { RoomConsumer } from '../context';
+// import Loading from './Loading';
+
+// const RoomsContainer = () => {
+// 	return (
+// 		<RoomConsumer>
+// 			{value => {
+// 				const { loading, sortedRooms, rooms } = value;
+// 				if (loading) {
+// 					return <Loading />;
+// 				}
+// 				return (
+// 					<div>
+// 						Hello from RoomsContainer
+// 						<RoomsFilter rooms={rooms} />
+// 						<RoomsList rooms={sortedRooms} />
+// 					</div>
+// 				);
+// 			}}
+// 		</RoomConsumer>
+// 	);
+// };
+
+// export default RoomsContainer;
