a1b6bb9111aee9e90d51d42eaf60535b75ed0a00
tayoolaigbe <tayoolaigbe@outlook.com>
2023-02-19 11:59:51 +0000

    Added filterRooms to the context.jsx to filter the rooms accordingly

--- resort/src/context.jsx
+++ resort/src/context.jsx
@@ -52,11 +52,12 @@ class RoomProvider extends Component {
 		return room;
 	};
 
+	//handleChange for the whole form
 	handleChange = event => {
 		const target = event.target;
 		const value = target.type === 'checkbox' ? target.checked : target.value;
 		const name = target.name;
-		console.log(name, value);
+		// console.log(name, value);
 
 		this.setState(
 			{
@@ -64,6 +65,42 @@ class RoomProvider extends Component {
 			},
 			this.filterRooms
 		);
+	};
+
+	filterRooms = () => {
+		let { rooms, type, capacity, price, minSize, maxSize, breakfast, pets } =
+			this.state;
+
+		let tempRooms = [...rooms];
+		// transform values
+		// get capacity
+		capacity = parseInt(capacity);
+		price = parseInt(price);
+		// filter by type
+		if (type !== 'all') {
+			tempRooms = tempRooms.filter(room => room.type === type);
+		}
+		// filter by capacity
+		if (capacity !== 1) {
+			tempRooms = tempRooms.filter(room => room.capacity >= capacity);
+		}
+		// filter by price
+		tempRooms = tempRooms.filter(room => room.price <= price);
+		//filter by size
+		tempRooms = tempRooms.filter(
+			room => room.size >= minSize && room.size <= maxSize
+		);
+		//filter by breakfast
+		if (breakfast) {
+			tempRooms = tempRooms.filter(room => room.breakfast === true);
+		}
+		//filter by pets
+		if (pets) {
+			tempRooms = tempRooms.filter(room => room.pets === true);
+		}
+		this.setState({
+			sortedRooms: tempRooms,
+		});
 	};
 
 	render() {
