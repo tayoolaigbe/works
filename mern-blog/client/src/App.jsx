58eb06cfb84a72864a8a4e26822e0ad3fe4622e7
tayoolaigbe <tayoolaigbe@outlook.com>
2023-02-22 20:59:19 +0000

    Added the pages directory to properly define my pagesánd extracted the LoginPage, RegisterPage and IndexPage

--- mern-blog/client/src/App.jsx
+++ mern-blog/client/src/App.jsx
@@ -1,16 +1,18 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Header from './Header';
-import Post from './Post';
 import Layout from './Layout';
+import IndexPage from './pages/IndexPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
 	return (
 		<Routes>
 			<Route path="/" element={<Layout />}>
-				<Route index element={<Post />} />
-				<Route path="/login" element={<div>Login to yours</div>} />
-				<Route path="/register" element={<div>Register an account</div>} />
+				<Route index element={<IndexPage />} />
+				<Route path="/login" element={<LoginPage />} />
+				<Route path="/register" element={<RegisterPage />} />
 			</Route>
 		</Routes>
 	);
