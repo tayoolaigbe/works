5176615d073ef1d27a81ff22a0cdb28a0fa446e6
tayoolaigbe <tayoolaigbe@outlook.com>
2023-02-22 20:16:41 +0000

    Extracted Post into its own component and used it in App.jsx

---
+++ mern-blog/client/src/Post.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Post = () => {
+	return (
+		<div className="post">
+			<div className="image">
+				<img
+					src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=1390&crop=1"
+					alt=""
+				/>
+			</div>
+			<div className="texts">
+				<h2>Full-house battery backup coming later this year</h2>
+				<p className="info">
+					<a className="author">Eyitayo Olaigbe</a>
+					<time>2023-02-20 22:45</time>
+				</p>
+				<p className="summary">
+					Today at its special launch event, home backup power giant EcoFlow
+					launched a flurry of new products, including a “Whole-Home Backup
+					Power Solution.”
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default Post;
