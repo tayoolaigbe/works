98eaa9ab9d66e495893d8080fbecdad852ade2aa
tayoolaigbe <tayoolaigbe@outlook.com>
2023-02-22 21:41:24 +0000

    Styles both the Login and Register forms and also added h1 to differentiate pages

--- mern-blog/client/src/pages/RegisterPage.jsx
+++ mern-blog/client/src/pages/RegisterPage.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const RegisterPage = () => {
 	return (
 		<form className="register">
+			<h1>Register</h1>
 			<input type="text" placeholder="username" />
 			<input type="password" placeholder="password" />
 			<button>Register</button>
