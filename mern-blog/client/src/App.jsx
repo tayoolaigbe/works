841df8eac00a58b6671de971ca1f0da0d46d0c78
tayoolaigbe <tayoolaigbe@outlook.com>
2023-02-22 20:32:56 +0000

    Added the index route in App and used BrowserRouter in main.jsx

--- mern-blog/client/src/App.jsx
+++ mern-blog/client/src/App.jsx
@@ -1,16 +1,24 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Header from './Header';
 import Post from './Post';
 
 function App() {
 	return (
-		<main>
-			<Header />
+		<Routes>
+			<Route
+				index
+				element={
+					<main>
+						<Header />
 
-			<Post />
-			<Post />
-			<Post />
-		</main>
+						<Post />
+						<Post />
+						<Post />
+					</main>
+				}
+			/>
+		</Routes>
 	);
 }
 
