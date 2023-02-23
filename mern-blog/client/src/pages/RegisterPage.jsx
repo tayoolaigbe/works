c8d2c76447deed466f8c3bc38bf277b8618bf7a1
tayoolaigbe <tayoolaigbe@outlook.com>
2023-02-23 22:23:09 +0000

    Installed cors to solve the cors error and used state to handle the forms inputs int the register function

--- mern-blog/client/src/pages/RegisterPage.jsx
+++ mern-blog/client/src/pages/RegisterPage.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const RegisterPage = () => {
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+
+	const register = async e => {
+		e.preventDefault();
+		await fetch('http://localhost:4000/register', {
+			method: 'POST',
+			body: JSON.stringify({ username, password }),
+			headers: { 'Content-Type': 'application/json' },
+		});
+	};
 	return (
-		<form className="register">
+		<form className="register" onSubmit={register}>
 			<h1>Register</h1>
-			<input type="text" placeholder="username" />
-			<input type="password" placeholder="password" />
-			<button>Register</button>
+			<input
+				type="text"
+				placeholder="username"
+				value={username}
+				onChange={e => setUsername(e.target.value)}
+			/>
+			<input
+				type="password"
+				placeholder="password"
+				value={password}
+				onChange={e => setPassword(e.target.value)}
+			/>
+			<button type="submit">Register</button>
 		</form>
 	);
 };
