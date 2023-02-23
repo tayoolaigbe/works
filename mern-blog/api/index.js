650aeb33ed0aa38f3f5a28931f54180b0a52d683
tayoolaigbe <tayoolaigbe@outlook.com>
2023-02-23 23:07:04 +0000

    Added dotenv to the backend to protect connection string and connected application to database

--- mern-blog/api/index.js
+++ mern-blog/api/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect(process.env.CONNECTION_URL);
 
 app.post('/register', (req, res) => {
 	const { username, password } = req.body;
