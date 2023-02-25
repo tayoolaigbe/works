1284f4d0a708fbfb48804caa9e2b8a56bee3f5df
tayoolaigbe <tayoolaigbe@outlook.com>
2023-02-25 16:14:31 +0000

    Added the onQueryStarted async function to  authAPiSlice.jsx

--- techNotes/frontend/src/features/auth/authApiSlice.jsx
+++ techNotes/frontend/src/features/auth/authApiSlice.jsx
@@ -1,5 +1,5 @@
 import { apiSlice } from '../../app/api/apiSlice';
-import { logOut } from './authSlice';
+import { logOut, setCredentials } from './authSlice';
 
 export const authApiSlice = apiSlice.injectEndpoints({
 	endpoints: builder => ({
@@ -18,7 +18,7 @@ export const authApiSlice = apiSlice.injectEndpoints({
 			async onQueryStarted(arg, { dispatch, queryFulfilled }) {
 				try {
 					const { data } = await queryFulfilled;
-					// console.log(data);s
+					// console.log(data);
 					dispatch(logOut());
 					setTimeout(() => {
 						dispatch(apiSlice.util.resetApiState());
@@ -33,6 +33,16 @@ export const authApiSlice = apiSlice.injectEndpoints({
 				url: '/auth/refresh',
 				method: 'GET',
 			}),
+			async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+				try {
+					const { data } = await queryFulfilled;
+					console.log(data);
+					const { accessToken } = data;
+					dispatch(setCredentials({ accessToken }));
+				} catch (err) {
+					console.log(err);
+				}
+			},
 		}),
 	}),
 });
