e5d89f3b500d18a67f7d5a3d9f84d3d62cb50fc8
tayoolaigbe <tayoolaigbe@outlook.com>
2023-02-26 17:54:35 +0000

    Completed the DasHeader component with th eproper buttons

--- techNotes/frontend/src/components/DashHeader.jsx
+++ techNotes/frontend/src/components/DashHeader.jsx
@@ -35,12 +35,6 @@ const DashHeader = () => {
 	const onNotesClicked = () => navigate('/dash/notes');
 	const onUsersClicked = () => navigate('/dash/users');
 
-	// const onLogOutClicked = () => sendLogout();
-
-	if (isLoading) return <p>Logging Out...</p>;
-
-	if (isError) return <p>Error: {error.data.message}</p>;
-
 	let dashClass = null;
 	if (
 		!DASH_REGEX.test(pathname) &&
@@ -50,24 +44,88 @@ const DashHeader = () => {
 		dashClass = 'dash-header__container--small';
 	}
 
+	let newNoteButton = null;
+	if (NOTES_REGEX.test(pathname)) {
+		newNoteButton = (
+			<button
+				className="icon-button"
+				title="New Note"
+				onClick={onNewNoteClicked}
+			>
+				<FontAwesomeIcon icon={faFileCirclePlus} />
+			</button>
+		);
+	}
+
+	let newUserButton = null;
+	if (USERS_REGEX.test(pathname)) {
+		newUserButton = (
+			<button
+				className="icon-button"
+				title="New User"
+				onClick={onNewUserClicked}
+			>
+				<FontAwesomeIcon icon={faUserPlus} />
+			</button>
+		);
+	}
+
+	let userButton = null;
+	if (isManager || isAdmin) {
+		if (!USERS_REGEX.test(pathname) && pathname.includes('/dash')) {
+			userButton = (
+				<button className="icon-button" title="Users" onClick={onUsersClicked}>
+					<FontAwesomeIcon icon={faUserGear} />
+				</button>
+			);
+		}
+	}
+
+	let notesButton = null;
+	if (!NOTES_REGEX.test(pathname) && pathname.includes('/dash')) {
+		notesButton = (
+			<button className="icon-button" title="Notes" onClick={onNotesClicked}>
+				<FontAwesomeIcon icon={faFilePen} />
+			</button>
+		);
+	}
+
 	const logoutButton = (
 		<button className="icon-button" title="Logout" onClick={sendLogout}>
 			<FontAwesomeIcon icon={faRightFromBracket} />
 		</button>
 	);
 
+	const errClass = isError ? 'errmsg' : 'offscreen';
+
+	let buttonContent;
+	if (isLoading) {
+		buttonContent = <p>Logging Out...</p>;
+	} else {
+		buttonContent = (
+			<>
+				{newNoteButton}
+				{newUserButton}
+				{notesButton}
+				{userButton}
+				{logoutButton}
+			</>
+		);
+	}
+
 	const content = (
-		<header className="dash-header">
-			<div className={`dash-header__container ${dashClass}`}>
-				<Link to="/dash">
-					<h1 className="dash-header__title">techNotes</h1>
-				</Link>
-				<nav className="dash-header__nav">
-					{/* add more buttons later */}
-					{logoutButton}
-				</nav>
-			</div>
-		</header>
+		<>
+			<p className={errClass}>{error?.data?.message}</p>
+
+			<header className="dash-header">
+				<div className={`dash-header__container ${dashClass}`}>
+					<Link to="/dash">
+						<h1 className="dash-header__title">techNotes</h1>
+					</Link>
+					<nav className="dash-header__nav">{buttonContent}</nav>
+				</div>
+			</header>
+		</>
 	);
 
 	return content;
