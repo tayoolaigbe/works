3c998e6416dcdee5d53ef909cadf494f95b7de20
tayoolaigbe <tayoolaigbe@outlook.com>
2023-03-08 18:40:48 +0000

    Created the routes directory and setup the Root component in there for the root path '/'

--- contacts/src/main.jsx
+++ contacts/src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './routes/root';
 import './index.css';
 
 const router = createBrowserRouter([
 	{
 		path: '/',
-		element: <div>Hello world</div>,
+		element: <Root />,
 	},
 ]);
 ReactDOM.createRoot(document.getElementById('root')).render(
