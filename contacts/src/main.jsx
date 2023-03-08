1f7bf6cb84df18632f9abd3b3a6ac1f500373ed7
tayoolaigbe <tayoolaigbe@outlook.com>
2023-03-08 19:38:50 +0000

    Added the loader and useLoaderData in the root and used the hook to get our data

--- contacts/src/main.jsx
+++ contacts/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Root from './routes/root';
+import Root, { loader as rootLoader } from './routes/root';
 import Contact from './routes/contact';
 import './index.css';
 import ErrorPage from './error-page';
@@ -11,6 +11,7 @@ const router = createBrowserRouter([
 		path: '/',
 		element: <Root />,
 		errorElement: <ErrorPage />,
+		loader: rootLoader,
 		children: [
 			{
 				path: 'contacts/:contactId',
--- contacts/src/routes/root.jsx
+++ contacts/src/routes/root.jsx
@@ -1,5 +1,13 @@
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet, Link, useLoaderData } from 'react-router-dom';
+import { getContacts } from '../contacts';
+
+export async function loader() {
+	const contacts = await getContacts();
+	return { contacts };
+}
+
 export default function Root() {
+	const { contacts } = useLoaderData();
 	return (
 		<>
 			<div id="sidebar">
@@ -21,14 +29,28 @@ export default function Root() {
 					</form>
 				</div>
 				<nav>
-					<ul>
-						<li>
-							<Link to={`/contacts/1`}>Adedamola</Link>
-						</li>
-						<li>
-							<Link to={`/contacts/2`}>Olabisi</Link>
-						</li>
-					</ul>
+					{contacts.length ? (
+						<ul>
+							{contacts.map(contact => (
+								<li key={contact.id}>
+									<Link to={`contacts/${contact.id}`}>
+										{contact.first || contact.last ? (
+											<>
+												{contact.first} {contact.last}
+											</>
+										) : (
+											<i>No Name</i>
+										)}{' '}
+										{contact.favorite && <span>★</span>}
+									</Link>
+								</li>
+							))}
+						</ul>
+					) : (
+						<p>
+							<i>No contacts</i>
+						</p>
+					)}
 				</nav>
 			</div>
 			<div id="detail">
