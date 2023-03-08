7144dbc1fd53229691733a15d14f4bee92352303
tayoolaigbe <tayoolaigbe@outlook.com>
2023-03-08 19:49:22 +0000

    Added the action function in root to handle create contact and used it in main.jsx

--- contacts/src/main.jsx
+++ contacts/src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Root, { loader as rootLoader } from './routes/root';
+import Root, {
+	loader as rootLoader,
+	action as rootAction,
+} from './routes/root';
 import Contact from './routes/contact';
 import './index.css';
 import ErrorPage from './error-page';
@@ -12,6 +15,7 @@ const router = createBrowserRouter([
 		element: <Root />,
 		errorElement: <ErrorPage />,
 		loader: rootLoader,
+		action: rootAction,
 		children: [
 			{
 				path: 'contacts/:contactId',
