6d34ebe0d256b89798609d9f61ea64e7e01107d0
tayoolaigbe <tayoolaigbe@outlook.com>
2023-03-09 09:45:21 +0000

    Used useNavigation hook to add global pending UI state

--- contacts/src/routes/root.jsx
+++ contacts/src/routes/root.jsx
@@ -3,6 +3,7 @@ import {
 	NavLink,
 	useLoaderData,
 	Form,
+	useNavigation,
 	redirect,
 } from 'react-router-dom';
 import { getContacts, createContact } from '../contacts';
@@ -19,6 +20,7 @@ export async function action() {
 
 export default function Root() {
 	const { contacts } = useLoaderData();
+	const navigation = useNavigation();
 	return (
 		<>
 			<div id="sidebar">
@@ -69,7 +71,10 @@ export default function Root() {
 					)}
 				</nav>
 			</div>
-			<div id="detail">
+			<div
+				id="detail"
+				className={navigation.state === 'loading' ? 'loading' : ''}
+			>
 				<Outlet />
 			</div>
 		</>
