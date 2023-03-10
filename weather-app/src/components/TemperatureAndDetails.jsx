f94d12d579a2419e50d4a643049dcb22decfcef9
tayoolaigbe <tayoolaigbe@outlook.com>
2023-03-10 21:55:39 +0000

    Working on the TemperatureAndDetails component

--- weather-app/src/components/TemperatureAndDetails.jsx
+++ weather-app/src/components/TemperatureAndDetails.jsx
@@ -1,7 +1,46 @@
 import React from 'react';
+import {
+	UilTemperature,
+	UilTear,
+	UilWind,
+	UilSun,
+	UilSunset,
+} from '@iconscout/react-unicons';
 
 const TemperatureAndDetails = () => {
-	return <div>TemperatureAndDetails</div>;
+	return (
+		<div>
+			<div className="flex items-center justify-center py-6 text-xl text-cyan-300">
+				<p>Cloudy</p>
+			</div>
+
+			<div className="flex flex-row items-center justify-between text-white py-3">
+				<img
+					src="http://openweatrhermap.org/img/wn/01d@2x.png"
+					alt=""
+					className="w-20"
+				/>
+				<p className="text-5xl">34°</p>
+				<div className="flex flex-col space-y-2">
+					<div className="flex font-light text-sm item-center justify-center">
+						<UilTemperature size={18} className="mr-1" />
+						Real feel:
+						<span className="font-medium ml-1">32°</span>
+					</div>
+					<div className="flex font-light text-sm item-center justify-center">
+						<UilTear size={18} className="mr-1" />
+						Humidity:
+						<span className="font-medium ml-1">65%</span>
+					</div>
+					<div className="flex font-light text-sm item-center justify-center">
+						<UilWind size={18} className="mr-1" />
+						Wind speed:
+						<span className="font-medium ml-1">11 km/h</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default TemperatureAndDetails;
