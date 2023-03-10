0db997aeafcb00727e5372168a8496d24ce1cf8b
tayoolaigbe <tayoolaigbe@outlook.com>
2023-03-10 22:06:03 +0000

    Completed the mock up for TemperatureAndDetails.jsx component

--- weather-app/src/components/TemperatureAndDetails.jsx
+++ weather-app/src/components/TemperatureAndDetails.jsx
@@ -34,10 +34,32 @@ const TemperatureAndDetails = () => {
 					</div>
 					<div className="flex font-light text-sm item-center justify-center">
 						<UilWind size={18} className="mr-1" />
-						Wind speed:
+						Wind:
 						<span className="font-medium ml-1">11 km/h</span>
 					</div>
 				</div>
+			</div>
+
+			<div className="flex flex-row items-center justify-center space-x-2 text-white text-sm py-3">
+				<UilSun />
+				<p className="font-light">
+					Rise: <span className="font-medium ml-1">06:45 AM</span>
+				</p>
+				<p className="font-light">|</p>
+				<UilSunset />
+				<p className="font-light">
+					Set: <span className="font-medium ml-1">07:35 PM</span>
+				</p>
+				<p className="font-light">|</p>
+				<UilSun />
+				<p className="font-light">
+					High: <span className="font-medium ml-1">45°</span>
+				</p>
+				<p className="font-light">|</p>
+				<UilSun />
+				<p className="font-light">
+					Low: <span className="font-medium ml-1">40°</span>
+				</p>
 			</div>
 		</div>
 	);
