6a3567ed2bfdf6ed51ce8163a6fdc35b7711b909
tayoolaigbe <tayoolaigbe@outlook.com>
2023-03-10 20:23:43 +0000

    Installed iconscout/unicons

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,7 +1,10 @@
 import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react';
 
 function App() {
-	return <div className="text-3xl font-bold underline">Hello world!</div>;
+	return (
+		<div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400"></div>
+	);
 }
 
 export default App;
