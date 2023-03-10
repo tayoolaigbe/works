68cb413f8b4e6f56dc836ecc2f2d660d34da86a2
tayoolaigbe <tayoolaigbe@outlook.com>
2023-03-10 20:54:30 +0000

    Working on the Search input and the icons

---
+++ weather-app/src/components/TopButtons.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const TopButtons = () => {
+	const cities = [
+		{ id: 1, title: 'London' },
+		{ id: 2, title: 'Sydney' },
+		{ id: 3, title: 'Tokyo' },
+		{ id: 4, title: 'Toronto' },
+		{ id: 5, title: 'Paris' },
+		{ id: 6, title: 'Lagos' },
+	];
+	return (
+		<div className="flex items-center justify-around my-6">
+			{cities.map(city => (
+				<button className="text-white text-lg font-medium" key={city.id}>
+					{city.title}
+				</button>
+			))}
+		</div>
+	);
+};
+
+export default TopButtons;
