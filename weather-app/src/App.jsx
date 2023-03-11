7ba4545a610f09c7aa63c337a9cb8e2ffcd0a1e7
tayoolaigbe <tayoolaigbe@outlook.com>
2023-03-11 18:47:20 +0000

    Added the formatBackground function to App.jsx to change background depending on temperature

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -6,7 +6,6 @@ import TemperatureAndDetails from './components/TemperatureAndDetails';
 import Forecast from './components/Forecast';
 import { useEffect, useState } from 'react';
 import getFormattedWeatherData from './services/weatherServices';
-import { data } from 'autoprefixer';
 
 function App() {
 	const [query, setQuery] = useState({ q: 'edinburgh' });
@@ -23,8 +22,18 @@ function App() {
 		fetchWeather();
 	}, [query, units]);
 
+	const formatBackground = () => {
+		if (!weather) return 'from-cyan-700 to-blue-700';
+		const threshold = units === 'metric' ? 20 : 60;
+		if (weather.temp <= threshold) return 'from-cyan-700 to-blue-700';
+
+		return 'from-yellow-700 to-orange-700';
+	};
+
 	return (
-		<div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+		<div
+			className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400 ${formatBackground()}`}
+		>
 			<TopButtons setQuery={setQuery} />
 			<Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
 			{weather && (
