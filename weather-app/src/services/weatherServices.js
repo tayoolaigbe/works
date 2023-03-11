2ab413293feb9252302a338ba33ecd10ca95614a
tayoolaigbe <tayoolaigbe@outlook.com>
2023-03-11 14:50:53 +0000

    Still working on the weatherServices.js functions

--- weather-app/src/services/weatherServices.js
+++ weather-app/src/services/weatherServices.js
@@ -7,3 +7,53 @@ const getWeatherData = (infoType, searchParams) => {
 
 	return fetch(url).then(res => res.json());
 };
+
+const formatCurrentWeather = data => {
+	const {
+		coord: { lat, lon },
+		main: { temp, feels_like, temp_min, temp_max, humidity },
+		name,
+		dt,
+		sys: { country, sunrise, sunset },
+		weather,
+		wind: { speed },
+	} = data;
+
+	const { main: details, icon } = weather[0];
+
+	return {
+		lat,
+		lon,
+		temp,
+		feels_like,
+		temp_min,
+		temp_max,
+		humidity,
+		name,
+		dt,
+		country,
+		sunrise,
+		sunset,
+		details,
+		icon,
+		speed,
+	};
+};
+
+const getFormattedWeatherData = async searchParams => {
+	const formattedCurrentWeather = await getWeatherData(
+		'weather',
+		searchParams
+	).then(formatCurrentWeather);
+
+	const { lat, lon } = formattedCurrentWeather;
+
+	const formattedForecastWeather = await getWeatherData('onecall', {
+		lat,
+		lon,
+		exclude: 'current, minutely, alerts',
+		units: searchParams.units,
+	});
+
+	return formatCurrentWeather;
+};
